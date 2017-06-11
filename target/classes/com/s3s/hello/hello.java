79fc820e1653840c625c533cb53bcc6b00b4f67d
User1 <faisal.aldayani@gmail.com>
2017-06-11 12:39:21 -0500

    "this is src file for helloworld"

---
+++ target/classes/com/s3s/hello/hello.java
@@ -0,0 +1,13 @@
+package com.s3s.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
